d2825335d84baa44fc582b5906acf503a2d8262b
MartinOrl <53393723+MartinOrl@users.noreply.github.com>
2020-02-03 11:25:50 +0100

    Updates

---
+++ precache-manifest.b17af490580fb33e4df57410d1acec64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8fd1af9846c5313a3e967eb54aa6ed",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ceb63c9b25d2e6c0d86a",
+    "url": "/monster-rolodex/static/css/main.483fee83.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb63c9b25d2e6c0d86a",
+    "url": "/monster-rolodex/static/js/main.25352ae1.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
